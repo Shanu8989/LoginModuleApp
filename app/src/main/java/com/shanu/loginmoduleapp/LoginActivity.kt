a7d0271217f9b1146c7ff6135d2b6452ee4c57f4
Shanu786 <shanu4s1332@gmail.com>
2024-03-06 23:50:42 +0530

    Implemented the bimetric module into Login module

--- app/src/main/java/com/shanu/loginmoduleapp/LoginActivity.kt
+++ app/src/main/java/com/shanu/loginmoduleapp/LoginActivity.kt
@@ -1,13 +1,17 @@
 package com.shanu.loginmoduleapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import com.shanu.biometricDialog.CustomBiometricDialog
+import com.shanu.biometricDialog.SettingsActivity
 import com.shanu.loginmoduleapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var dialog: SettingsActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -15,6 +19,21 @@ class LoginActivity : AppCompatActivity() {
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setUI()
     }
+
+    private fun setUI() {
+        val userName = binding.username.text.toString()
+        val password = binding.password.text.toString()
+
+        if (fieldsNullOrEmpty(userName, password)) {
+            Toast.makeText(this, "All fields are mandatory", Toast.LENGTH_SHORT).show()
+        } else {
+            dialog.callCustomBiometricDialog()
+        }
+    }
+
+    private fun fieldsNullOrEmpty(vararg data: String?): Boolean = data.any { it.isNullOrEmpty() }
 
 }
