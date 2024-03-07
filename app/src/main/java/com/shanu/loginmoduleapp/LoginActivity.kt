857bd25a5f230e185f4452b38281eaaebe950437
Shanu786 <shanu4s1332@gmail.com>
2024-03-07 11:43:26 +0530

    added biometric click in login module

--- app/src/main/java/com/shanu/loginmoduleapp/LoginActivity.kt
+++ app/src/main/java/com/shanu/loginmoduleapp/LoginActivity.kt
@@ -5,13 +5,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import com.shanu.biometricDialog.CustomBiometricDialog
-import com.shanu.biometricDialog.SettingsActivity
 import com.shanu.loginmoduleapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var dialog: SettingsActivity
+    private lateinit var customDialog: CustomBiometricDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -31,9 +30,21 @@ class LoginActivity : AppCompatActivity() {
             if (fieldsNullOrEmpty(userName, password)) {
                 Toast.makeText(this, "All fields are mandatory...", Toast.LENGTH_SHORT).show()
             } else {
-                dialog = SettingsActivity()
+                invokeCustomDialog()
             }
         }
+    }
+
+    private fun invokeCustomDialog() {
+        customDialog =
+            CustomBiometricDialog.Builder().isCancelable(true).onBiometricClick {
+                Toast.makeText(this, "Clicked on Biometric Dialog.", Toast.LENGTH_SHORT).show()
+            }.build()
+
+        customDialog.show(
+            this.supportFragmentManager, "CustomBiometricDialog"
+        )
+
     }
 
     private fun fieldsNullOrEmpty(vararg data: String?): Boolean = data.any { it.isNullOrEmpty() }
